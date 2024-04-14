3ce7da4d8e8fe4c9c20e846d1e731ebd9268c11e
unsubstantiated-Script <jusfulton@gmail.com>
2024-04-13 18:14:08 -0700

    Cards in grid done

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from streamlit_extras.grid import grid
 
 st.set_page_config(layout="wide")
 
+# Squirrly, but trying to stop the "full screen" button from appearing as it doesn't align well in grids.
+hide_full_screen = '''
+<style>
+p {font-size: 1.5rem;}
+.element-container .st-emotion-cache-youdgo {visibility: hidden;}
+</style>
+'''
+st.markdown(hide_full_screen, unsafe_allow_html=True)
+
 col1, col2 = st.columns(2)
 
 with col1:
     st.image('img/profile_photo.png', use_column_width=True)
 
 with col2:
-    st.title("Justin C. Fulton")
+    st.title("Justin C. Fulton", anchor=False)
     content = """
 I'm a software engineer, educator, content development professional, and administrator; leveraging expertise gained over 
 many years of work in the education sector as well as advanced software design for insurance and real estate agencies. 
@@ -27,11 +36,13 @@ Below you can find some of the apps I have built in GoLang and Python. Feel free
 """
 st.header(content2, divider="blue")
 
-col3, col4 = st.columns(2)
-
 df = pandas.read_csv("data.csv", sep=";")
 
-my_grid = grid(2, 2)
+my_grid = grid(3, gap="large")
 
 for index, row in df.iterrows():
-    my_grid.header(row["title"])
+    tile = my_grid.container()
+    tile.header(row["title"], anchor=False)
+    tile.write(row["description"])
+    tile.image("img/" + row["image"], use_column_width='always')
+    tile.write(f"[Source Code]({row['url']})")
