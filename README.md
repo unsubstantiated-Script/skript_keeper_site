ca20a1f8b8bea588238957f9a7c906c9900a88b6
unsubstantiated-Script <jusfulton@gmail.com>
2024-04-14 19:09:05 -0700

    Site complete and secure

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A portfolio website to showcase work done in Python and GoLang
 
 Data to populate site reads from CSV to keep things super simple to edit and app stays lightweight.
+
+Looking to deploy via Render. 
---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+import os
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = "unsubstantiated.script@gmail.com"
+    # Note to self. Password sits in .zshrc and awaits deployment for environment there.
+    password = os.getenv("GMAIL_PASSWORD")
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, username, message)
+        server.quit()
+        print('Email sent!')
